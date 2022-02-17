654cdbecec40c6055f9ed9bce53efcf69703f57e
franckymougel <francky.mougel@gmail.com>
2022-02-16 21:55:45 -0500

    Cretation de la gestion des fichiers modeles

---
+++ testFichier.Model/FichierModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace testFichier.Model
+{
+    public class FichierModel
+    {
+        public string Nom { get; set; }
+        public object MyProperty { get; set; }
+    }
+}
