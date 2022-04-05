7e237866510b473cbede0c98c0239c8a1ff8d248
franckymougel <francky.mougel@gmail.com>
2022-04-05 14:53:32 -0400

    ajout fichier

---
+++ testFichier.Model/Constantes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testFichier.Model
+{
+    public class Constantes
+    {
+        public const string REPERTOIRE = "model/";
+        public static string[] LISTE_FORMAT = { "Texte", "Date", "Numérique" };
+    }
+}
